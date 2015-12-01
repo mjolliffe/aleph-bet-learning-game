ba1b212c482767fa449d5c2c7b4eefdf71f9af70
Michelle Jolliffe <michelle_jolliffe@alumni.brown.edu>
2015-11-30 18:35:50 -0800

    Changed image colors. Trying different logic for game.

--- script.js
+++ script.js
@@ -1,86 +1,72 @@
-var words = ["shin", "bet", "tav", "aleph"];
-var images = ["images/shin.png", "images/bet.png", "images/tav.png", "images/aleph.png"];
+var words = [
+{ word: "shin",
+  selected: false
+},
+{ word: "bet",
+  selected: false
+},
+{ word: "tav",
+  selected: false
+},
+{ word: "aleph",
+  selected: false
+}];
+
+var images = [
+{ word: "shin",
+  selected: false,
+  path: "images/shin.png"
+},
+{ word: "bet",
+  selected: false,
+  path: "images/bet.png"
+},
+{ word: "tav",
+  selected: false,
+  path: "images/tav.png"
+},
+{ word: "aleph",
+  selected: false,
+  path: "images/aleph.png"
+}];
+
 var correctPairs = 0;
-
-/* Image function used to create images and push to array
-var createImage = function(src) {
-  var img = new Image();
-  img.src = src;
-  return img;
-};
-
-// Push images of words and letter forms into images array
-images.push(createImage("images/shin.png"));
-images.push(createImage("images/bet.png"));
-images.push(createImage("images/tav.png"));
-images.push(createImage("images/aleph.png"));
-*/
 
 function displayAllWords() {
   for (var i=0;i<words.length;i++) {
-     document.getElementsByClassName("card")[i].innerHTML = words[i];
+    document.getElementsByClassName("card")[i].innerHTML = words[i].word;
+    document.getElementsByClassName("card")[i].addEventListener("click", setSelected);
   }
 }
 
 function displayAllImages() {
   for (var i=0;i<images.length;i++) {
-    document.getElementsByClassName("card2")[i].innerHTML = '<img src='+images[i]+'>';
+    document.getElementsByClassName("card2")[i].innerHTML = '<img src='+ images[i].path +' width="100px">';
+    document.getElementsByClassName("card2")[i].addEventListener("click", setSelected);
   }
 }
-
-
-for (var w = 0; w < words.length; w++) {
-  if (words[w] === images[w]) {
-     correctPairs += 1;
-     words.style.background = "red";
-     images.style.background = "red";
-  }
-
-}
-
 displayAllWords();
 displayAllImages();
-/*
-//Compare words to images
-function compare(){
-  if (words.indexOf('shin') === images.indexOf('images/shin.png')){
+
+function setSelected (){
+ for (var i = 0; i < words.length; i++) {
+  if (words[i].word == images[i].word) {
+    document.getElementsByClassName("card")[i].innerHTML.style.backgroundColor = "white";
+    document.getElementsByClassName("card2")[i].innerHTML.style.backgroundColor = "white";
     correctPairs += 1;
-  } else if (words.indexOf('bet') === images.indexOf('images/bet.png')){
-    correctPairs += 1;
-  }
-
+    }
+}
 }
 
-//Higlight images when clicked
-function highlightImage () {
-
+function choseAvatar(){
+  document.getElementsByClassName("kid").addEventListener("click", setSelectedAvatar);
 }
 
-/*
-Array.prototype.shuffle = function() {
-  var i = this.length, mix, temp;
-  while (i > 0) {
-    mix = Math.floor(Math.random() * (i+1));
-    temp = this[mix];
-    this[mix] = this [i];
-    this[i] = mix;
-  }
-}
-*/
-
-/*randomize
-var selected = [];
-for (var i = 0; i < 22; i++) {
-    // Randomly pick one from the array of faces
-    var randomInd = floor(random(images.length));
-    var image = images[randomInd];
-    // Push a copy onto array
-    selected.push(image);
-    // Remove from faces array so we don't re-pick
-    images.splice(randomInd, 1);
+function setSelectedAvatar (){
+  document.getElementsByClassName("kid").style.backgroundColor = "#173599";
 }
 
-randomize
-selected.sort(function() {
-    return 0.5 - random();
-});*/
+function reset (){
+  displayAllWords();
+  displayAllImages();
+}
